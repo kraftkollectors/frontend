4e80941e2e0dde42895e4641696a29b92b504ec0
Treasure56 <simplicitytreasure@gmail.com>
2024-06-27 16:45:28 +0100

    completed admin auth pages

--- src/app/(main)/(admin)/admin/users/Table.tsx
+++ src/app/(main)/(admin)/admin/users/Table.tsx
@@ -2,32 +2,46 @@ import { UserDetails } from "@/utils/types/user";
 import TableRow from "./TableRow";
 
 export default function Table() {
-    return (
-        <table>
-            <tbody>
-            <tr className="bg-black-50 py-3 font-bold">
-                <th>Email</th>
-                <th>Fullname</th>
-                <th>Username</th>
-                <th>Created</th>
-                <th>Accout Type</th>
-                <th>Active Payment</th>
-                <th></th>
-
-            </tr>
-                <TableRow 
-                {...{
-                    firstName: "Kesh",
-                    lastName:"Maduakolam",
-                    email: "john@gmail.com",
-                    _id: "nj5jjjtjkjtkkkk",
-                    userName: "kesh",
-                    createdAt: "10-12-2023 12:02:45",
-                    isArtisan: true,
-                } as UserDetails}
-                 />
-            </tbody>
-    
-        </table>
-    );
+  return (
+    <div className=" overflow-x-auto w-full">
+      <table className="min-w-[800px] w-full rounded-md overflow-hidden app-table">
+        <tbody>
+          <tr className="bg-black-50 font-bold rounded-md rounded-br-md rounded-bl-md">
+            <td className="py-2">Email</td>
+            <td>Fullname</td>
+            <td>Username</td>
+            <td>Created</td>
+            <td>Accout Type</td>
+            <td>Active Payment</td>
+            <td></td>
+          </tr>
+          <tr><td className="p-2"></td></tr>
+          <TableRow
+            {...{
+              firstName: "Kesh",
+              lastName: "Maduakolam",
+              email: "john@gmail.com",
+              _id: "nj5jjjtjkjtkkkk",
+              userName: "kesh",
+              createdAt: "10-12-2023 12:02:45",
+              isArtisan: true,
+            } as UserDetails
+            }
+          />
+          <TableRow
+            {...{
+              firstName: "Kesh",
+              lastName: "Maduakolam",
+              email: "john@gmail.com",
+              _id: "nj5jjjtjkjtkkkk",
+              userName: "kesh",
+              createdAt: "10-12-2023 12:02:45",
+              isArtisan: false,
+            } as UserDetails
+            }
+          />
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- src/app/(main)/(admin)/admin/users/TableRow.tsx
+++ src/app/(main)/(admin)/admin/users/TableRow.tsx
@@ -4,48 +4,36 @@ import { fullName } from "@/functions/helpers";
 import { UserDetails } from "@/utils/types/user";
 import OptionsPopOver from "./OptionsPopOver";
 
-export default function TableRow({_id, email, firstName, createdAt, isArtisan, userName, lastName   }:UserDetails) {
-    
-    return (
-        <tr className=" text-black-400 font-semibold text-label">
-            <td>
-                <p className="py-1">
-                    {email}
-                </p>
-            </td>
-            <td>
-                <p className="py-1">
-                    {fullName(firstName, lastName)}
-                </p>
-            </td>
-            <td>
-                <p className="py-1">
-                   @{userName}
-                </p>
-            </td>
-            <td>
-                <p className="py-1">
-                    {formatDate(createdAt)}
-                </p>
-            </td>
-            <td>
-                <p className="py-1">
-                    {isArtisan? 
-                    <span className="text-[#599f79] inline-flex">Verified <AppIcons.Verified /></span>:
-                    <span className="text-[#a87b3b] bg-[#FAF1E5] p-1 rounded-md">unverifed</span>
-                    }
-                </p>
-            </td>
-            <td>
-                <p className="py-1">
-                   none
-                </p>
-            </td>
-            <td>
-                <p className="py-1">
-                   <OptionsPopOver id={_id}/>
-                </p>
-            </td>
-        </tr>
-    );
+export default function TableRow({
+  _id,
+  email,
+  firstName,
+  createdAt,
+  isArtisan,
+  userName,
+  lastName,
+}: UserDetails) {
+  return (
+    <tr className=" text-black-400 font-semibold text-label bg-light border-b first-of-type:bg-red-400 typ">
+      <td className="py-2">{email}</td>
+      <td className="py-1">{fullName(firstName, lastName)}</td>
+      <td className="py-1">@{userName}</td>
+      <td className="py-1">{formatDate(createdAt)}</td>
+      <td className="py-1">
+        {isArtisan ? (
+          <span className="text-[#599f79] inline-flex items-center gap-2">
+            Verified <AppIcons.Verified />
+          </span>
+        ) : (
+          <span className="text-[#a87b3b] bg-[#FAF1E5] p-1 rounded-md">
+            unverifed
+          </span>
+        )}
+      </td>
+      <td className="py-1">none</td>
+      <td className="py-1">
+        <OptionsPopOver id={_id} />
+      </td>
+    </tr>
+  );
 }
