dd1d4f303ccb937bbe03f40670c5464260bb5c02
abundiko <abundiko111@gmail.com>
2024-06-28 09:51:14 +0100

    completed admin dashboard users page

--- src/app/(main)/(admin)/admin/users/page.tsx
+++ src/app/(main)/(admin)/admin/users/page.tsx
@@ -2,15 +2,22 @@ import { AdminSearch, PageTitle } from "@/components/admin";
 import GridRows from "./GridRows";
 import Filters from "./components/Filters";
 import Table from "./Table";
+import { redirect } from "next/navigation";
+import { paths } from "@/utils";
 
 export default function Page() {
+    async function search(formData: FormData){
+        'use server'
+        redirect(`${paths.adminUsers}?search=${formData.get('search')}`)
+    }
+    
     return (
         <div className="py-8 flex flex-col gap-4 w-full">
             <PageTitle>Users</PageTitle>
                <GridRows />
 
             <div className="flex justify-between max-lg:flex-col gap-2">
-               <AdminSearch action={() => console.log('hi')} />
+               <AdminSearch action={search} />
                <Filters />
             </div>
             <Table />
