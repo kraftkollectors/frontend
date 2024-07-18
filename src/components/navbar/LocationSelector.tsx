679c3239c7d66a4aa5ddf1951e3a0197b7247d26
Treasure56 <simplicitytreasure@gmail.com>
2024-07-18 14:32:05 +0100

    editing search

---
+++ src/components/navbar/LocationSelector.tsx
@@ -0,0 +1,13 @@
+import { MdMyLocation } from "react-icons/md";
+
+export default function NavLocationSelector() {
+    return (
+        <div className="relative flex gap-1 max-md:w-full items-center justify-stretch text-blak-300 px-1.5 font-semibold text-black-400 text-label">
+            <MdMyLocation className="flex-shrink-0" />
+            <input type="text" name="location" 
+            defaultValue={"Lagos, Nigeria"}
+            placeholder={"Lagos, Nigeria"}
+            className="text-black-400 !border-none !outline-none flex w-full py-1 md:!p-0 focus:!shadow-none focus:!border-none focus:!ring-transparent focus:!outline-none md:w-28 [outline:1px_solid_transparent!important] [all:unset] [box-shadow:none!important]" />
+        </div>
+    );
+}
--- src/components/navbar/NavSearch.tsx
+++ src/components/navbar/NavSearch.tsx
@@ -5,6 +5,7 @@ import { submitSearch } from "@/actions";
 import { FormButton } from "../ui/FormButton";
 import { Suspense } from "react";
 import { useParams } from "next/navigation";
+import NavLocationSelector from "./LocationSelector";
 
 export default function NavSearch() {
   const {query} = useParams();
@@ -12,20 +13,22 @@ export default function NavSearch() {
   
   return (
     <div className="relative flex items-center gap-2 w-full">
-      <form action={submitSearch} className="flex gap-2 items-center flex-grow rounded-md border p-1 ps-2 ">
-        <CiSearch className="flex-shrink-0" />
+      <form action={submitSearch} className="flex max-md:flex-col items-stretch flex-grow rounded-lg border border-black-50 overflow-hidden max-md:p-2 max-md:gap-2">
+        <select name="type" id="" className="bg-[#F0F0F0] max-md:hidden focus:outline-none text-black-400 border-none outline-none font-semibold text-label">
+          <option value="search">Services</option>
+          <option value="artisans">Artisans</option>
+        </select>
         <input
           type="text"
-          className="[border:1px_solid_transparent!important] flex-grow [outline:1px_solid_transparent!important] [all:unset] [box-shadow:none!important] w-[100%!important] text-dark-gray  px-3 py-1 rounded-md "
-          placeholder="Search..."
+          className="[border:1px_solid_transparent!important] font-semibold text-label text-black-500 flex-grow [outline:1px_solid_transparent!important] [all:unset] [box-shadow:none!important] w-[100%!important] md:!px-3 py-1 rounded-md "
+          placeholder="What are you looking for?"
           name="query"
           defaultValue={q}
         />
-        <FormButton className="btn-primary-tiny px-3 py-1 h-full flex-shrink-0 max-md:hidden">
-          Search
-        </FormButton>
+        <div className="max-md:self-center md:my-1 rounded max-md:h-[1px] md:w-[1px] w-[calc(100%-8px)] bg-black-100 md:bg-black-300"></div>
+        <NavLocationSelector />
       </form>
-      <Suspense><SearchMobile /></Suspense>
+      {/* <Suspense><SearchMobile /></Suspense> */}
     </div>
   );
 }
