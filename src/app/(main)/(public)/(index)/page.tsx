60349a26def198f373bdd29077b79938c214cedc
abundiko111 <abundiko111@gmail.com>
2024-07-25 13:34:01 +0000

    added category section

--- src/app/(main)/(public)/(index)/page.tsx
+++ src/app/(main)/(public)/(index)/page.tsx
@@ -2,12 +2,13 @@ import Hero from "./Hero";
 import MobileApp from "./MobileApp";
 import Banner from "./Banner";
 import FeaturedServices from "./FeaturedServices";
+import AllCategories from "./AllCategories";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* <AllCategories /> */}
+      <AllCategories />
       <FeaturedServices />
       <MobileApp />
       <Banner />
