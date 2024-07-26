eee92aa8d8c1677ae13053911d9c3fb52dac67c5
abundiko111 <abundiko111@gmail.com>
2024-07-26 08:09:26 +0000

    working on Featured artisans section of home page

--- src/app/(main)/(public)/(index)/page.tsx
+++ src/app/(main)/(public)/(index)/page.tsx
@@ -3,12 +3,14 @@ import MobileApp from "./MobileApp";
 import Banner from "./Banner";
 import FeaturedServices from "./FeaturedServices";
 import AllCategories from "./AllCategories";
+import FeaturedArtisans from "./FeaturedArtisans";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <AllCategories />
+      <FeaturedArtisans />
       <FeaturedServices />
       <MobileApp />
       <Banner />
