93fe89d097aecf4a1d88ca501bc12a394dcaafb3
abundiko <abundiko111@gmail.com>
2024-07-29 13:58:46 +0100

    worked on home page optimizations

--- src/app/(main)/(dashboard)/dashboard/(home)/layout.tsx
+++ src/app/(main)/(dashboard)/dashboard/(home)/layout.tsx
@@ -3,25 +3,21 @@ import DashBoardNav from "./DashBoardNav";
 import ProfileCard from "./ProfileCard";
 import ProfileCategories from "./ProfileCategories";
 
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-
   return (
-    <main className="grid md:grid-cols-10 md:app-container py-10 bg-light md:bg-light-text gap-6">
-      <div className="col-span-1 bg-light app-container md:hidden">
+    <main className="md:app-container grid gap-6 bg-light py-10 md:grid-cols-10 md:bg-light-text">
+      <div className="app-container col-span-1 bg-light md:hidden">
         <DashboardProfile />
       </div>
-      <div className="col-span-3 flex flex-col gap-4 max-md:hidden">
-        <ProfileCard  />
+      <div className="col-span-4 flex flex-col gap-4 max-md:hidden">
+        <ProfileCard />
         <ProfileCategories />
       </div>
-      <div className="col-span-full  md:col-span-7 flex flex-col gap-2 max-md:bg-light-text">
-        <div className="bg-light border-b md:border w-full">
+      <div className="col-span-full flex flex-col gap-2 max-md:bg-light-text md:col-span-6">
+        <div className="w-full border-b bg-light md:border">
           <DashBoardNav />
         </div>
-        <aside className=" max-md:app-container">
-        {children}
-        </aside>
+        <aside className="max-md:app-container">{children}</aside>
       </div>
     </main>
   );
