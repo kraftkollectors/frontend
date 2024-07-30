e775c80ef0be120f2c04a117ff193f95e6efb3e6
abundiko <abundiko111@gmail.com>
2024-07-30 08:51:18 +0100

    worked on service views count

--- src/components/dashboard/DeleteServiceModal.tsx
+++ src/components/dashboard/DeleteServiceModal.tsx
@@ -1,59 +1,85 @@
 /* eslint-disable @next/next/no-img-element */
-'use client'
+"use client";
 
-import { deleteService } from '@/actions';
-import { Dialog } from '@radix-ui/themes'
-import { useLayoutEffect, useState } from 'react';
-import { useFormState } from 'react-dom';
+import { deleteService } from "@/actions";
+import { Dialog } from "@radix-ui/themes";
+import { useLayoutEffect, useState } from "react";
+import { useFormState } from "react-dom";
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { FormButton } from '../ui/FormButton';
-import UserAuth from '../server/UserAuth';
-import { useRouter } from 'next/navigation';
-import { toast } from 'react-toastify';
-import AppToast from '../Toast';
+import { FormButton } from "../ui/FormButton";
+import UserAuth from "../server/UserAuth";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import AppToast from "../Toast";
 
+export default function DeleteServiceModal({
+  _id,
+  title,
+  description,
+  coverPhoto,
+}: {
+  title: string;
+  _id: string;
+  description: string;
+  coverPhoto: string;
+}) {
+  const { refresh } = useRouter();
+  const [open, setOpen] = useState(false);
+  const [res, action] = useFormState(deleteService, {});
+  useLayoutEffect(() => {
+    if (res.success) {
+      toast(<AppToast.success message={res.success} />);
+      setOpen(false);
+    } else if (res.error) {
+      toast(<AppToast.error message={res.error} />);
+    }
+  }, [res]);
 
-export default function DeleteServiceModal({ _id, title, description, coverPhoto }: { title: string; _id: string; description: string, coverPhoto:string }) {
-    const {refresh} = useRouter();
-    const [open, setOpen] = useState(false);
-    const [res, action] = useFormState(deleteService, {});
-    useLayoutEffect(() => {
-        if (res.success) {
-            toast(<AppToast.success message={res.success} />);
-            setOpen(false);
-        }else if(res.error){
-            toast(<AppToast.error message={res.error} />);
-        }
-    }, [res])
-
-    return (
-        <Dialog.Root open={open} onOpenChange={setOpen}>
-            <Dialog.Trigger>
-                <button className="delete-btn">
-                    <RiDeleteBin6Line />
-                    Delete
-                </button>
-            </Dialog.Trigger>
-            <Dialog.Content style={{
-                maxWidth: '500px',
-            }}>
-                <form action={action} className="flex flex-col gap-4">
-                    <h2 className="font-semibold text-sm">Do you want to delete this post?</h2>
-                    <div className="flex gap-2 border p-2 rounded">
-                        <img src={coverPhoto} alt={title} className="profile-img rounded overflow-hidden h-20" />
-                        <div>
-                        <h2 className="font-bold text-black-700">{title}</h2>
-                        <p className='text-black-400 line-clamp-3'>{description}</p>
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-2 gap-4">
-                        <Dialog.Close><button className="btn-dark-border py-2 !text-body" role='button'>Cancel</button></Dialog.Close>
-                        <FormButton className="delete-btn py-2 !border-danger !border" role='button'>Yes, Delete</FormButton>
-                    </div>
-                    <input type="hidden" name="_id" value={_id} />
-                    <UserAuth />
-                </form>
-            </Dialog.Content>
-        </Dialog.Root>
-    );
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger>
+        <button className="delete-btn">
+          <RiDeleteBin6Line />
+          Delete
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Content
+        style={{
+          maxWidth: "500px",
+        }}
+      >
+        <form action={action} className="flex flex-col gap-4">
+          <h2 className="text-sm font-semibold">
+            Do you want to delete this post?
+          </h2>
+          <div className="flex gap-2 rounded border p-2">
+            <img
+              src={coverPhoto}
+              alt={title}
+              className="profile-img aspect-[5/4] w-4/12 flex-shrink-0 overflow-hidden rounded"
+            />
+            <div>
+              <h2 className="font-bold text-black-700">{title}</h2>
+              <p className="line-clamp-3 text-black-400">{description}</p>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <Dialog.Close>
+              <button className="btn-dark-border py-2 !text-body" role="button">
+                Cancel
+              </button>
+            </Dialog.Close>
+            <FormButton
+              className="delete-btn !border !border-danger py-2"
+              role="button"
+            >
+              Yes, Delete
+            </FormButton>
+          </div>
+          <input type="hidden" name="_id" value={_id} />
+          <UserAuth />
+        </form>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
 }
--- src/components/dashboard/ServiceViews.tsx
+++ src/components/dashboard/ServiceViews.tsx
@@ -8,7 +8,7 @@ export default async function ServiceViews({ id }: { id: string }) {
   if (!totalViews || totalViews == "error") return null;
 
   return (
-    <div className="flex items-center justify-end gap-2 text-label font-semibold text-black-400 max-md:w-full">
+    <div className="flex items-center justify-end gap-2 text-label font-semibold text-black-400 max-md:w-full max-md:pt-4">
       <FaEye />
       <span>
         {totalViews.views} ({views.views} today)
