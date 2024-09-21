0dfef6c07620bd0be217dccb41be0bfc5f917fe0
abundiko <abundiko111@gmail.com>
2024-09-21 23:41:33 +0100

    editted service file upload and chat show service

--- dev.sh
+++ dev.sh
@@ -2,8 +2,12 @@ git remote remove origin
 
 git remote add origin https://github.com/abundiko/kraft.git
 
+git checkout dev
+
+git merge production
+
 git add .
 
-git commit -m "edited next config to accept bigger files"
+git commit -m "editted service file upload and chat show service"
 
 git push origin dev --force
